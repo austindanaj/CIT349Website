f559ba86f27bc9467db59ad734fa5ced1788476f
Austin Danaj <austin3soccer@aol.com>
2017-04-06 16:17:34 -0400

    Added zip functions

---
+++ CIT349Website/CIT349Website/Downloads.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Downloads" Language="C#" Debug="true" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Downloads.aspx.cs" Inherits="CIT349Website.Downloads" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>CIT349 Project</h1>
+        <p class="lead">Want to post to the blog, click the button below. Want to comment on a blog post, click the blog from below.</p>
+       <asp:Button ID="btnPost" runat="server" CssClass="btn btn-primary btn-lg" Text="Write Blog" /> 
+    </div>
+
+        <asp:Button ID="btnDownload" runat="server" OnClick="didCheckDownload" Text="Check Process 1" />
+   
+</asp:Content>
---
+++ CIT349Website/CIT349Website/Downloads.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO.Compression;
+using System.IO;
+using Renci.SshNet;
+
+namespace CIT349Website
+{
+
+    public partial class Downloads : System.Web.UI.Page
+    {
+        //SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ourdb"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+        public void sendCommand(string newcommand)
+        {
+            var PasswordConnection = new PasswordAuthenticationMethod("djmadurs", "r4vedave!");
+            string myData = null;
+            var connecitonInfo = new ConnectionInfo("141.210.25.85", "djmadurs", PasswordConnection);
+            using (SshClient ssh = new SshClient(connecitonInfo))
+            {
+                ssh.Connect();
+                var command = ssh.RunCommand(newcommand);
+                myData = command.Result;
+                ssh.Disconnect();
+            }
+
+        }
+
+
+        protected void didCheckDownload(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists("~/Downloads/Minecraft_Forge.zip"))
+                {
+                    Response.ContentType = "File/zip";
+                    Response.AppendHeader("Content-Disposition", "attachment; filename=Minecraft_Forge.zip");
+                    Response.TransmitFile(Server.MapPath("~/Downloads/Minecraft_Forge.zip"));
+                    Response.End();
+                }
+                else
+                {
+                    var PasswordConnection = new PasswordAuthenticationMethod("djmadurs", "r4vedave!");
+                    string myData = null;
+                    var connecitonInfo = new ConnectionInfo("141.210.25.85", "djmadurs", PasswordConnection);
+                    sendCommand("tar -czvf Minecraft_Forge.tar.gz /home/djmadurs/whitelist.json");
+                    using (var sftp = new SftpClient(connecitonInfo))
+                    {
+                        string localFileName = Path.GetDirectoryName("~/Downloads");
+                        string remoteFile = "";
+                        sftp.Connect();
+                        Stream file1 = File.OpenWrite(localFileName);
+
+                        sftp.DownloadFile("/home/djmadurs/whitelist.json", file1);
+                        sftp.Disconnect();
+                    }
+
+
+
+                }
+            }catch(Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+           
+        }
+        private static void DownloadDirectory(SftpClient client, string source, string destination)
+        {
+            var files = client.ListDirectory(source);
+            foreach (var file in files)
+            {
+                if (!file.IsDirectory && !file.IsSymbolicLink)
+                {
+                    DownloadFile(client, file, destination);
+                }
+                else if (file.IsSymbolicLink)
+                {
+                    Console.WriteLine("Ignoring symbolic link {0}", file.FullName);
+                }
+                else if (file.Name != "." && file.Name != "..")
+                {
+                    var dir = Directory.CreateDirectory(Path.Combine(destination, file.Name));
+                    DownloadDirectory(client, file.FullName, dir.FullName);
+                }
+            }
+        }
+        private static void DownloadFile(SftpClient client, Renci.SshNet.Sftp.SftpFile file, string directory)
+        {
+            Console.WriteLine("Downloading {0}", file.FullName);
+            using (Stream fileStream = File.OpenWrite(Path.Combine(directory, file.Name)))
+            {
+                client.DownloadFile(file.FullName, fileStream);
+            }
+        }
+       
+
+    }
+}
